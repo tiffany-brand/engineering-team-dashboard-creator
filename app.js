0a6f2fc8503cc9523525ffc0b16d3f5f84717c41
Tiffany Brand <brand2learn@gmail.com>
2020-08-23 21:38:13 -0400

    Begin buildTeam logic

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const questions = require('./lib/questions');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
-
+const render = require('./lib/htmlRenderer');
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const employees = [];
+
+// function to prompt user - returns answers object
+const promptUser = (type) => {
+	return inquirer.prompt(questions[type]);
+};
+
+const buildTeam = () => {
+	promptUser('manager').then((emp) => {
+		const newEmp = new Manager(emp.name, emp.id, emp.email, emp.officeNumber);
+		console.log(newEmp);
+		employees.push(newEmp);
+		console.log(`Employees: ${employees}`);
+	});
+};
+
+buildTeam();
+
+// prompt for manager questions
+// push Manager onto array
+//ask for next employee
+// if engineer, prompt for engineer questions
+// if intern, prompt for intern questions
+// if done, render html
+// write html file
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
