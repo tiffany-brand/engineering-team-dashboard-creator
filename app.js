bffa340427b3db41869f88efdf7d7ec871f090b8
Tiffany Brand <brand2learn@gmail.com>
2020-08-27 21:08:01 -0400

    Remove console.logs

--- app.js
+++ app.js
@@ -47,11 +47,9 @@ const askForNext = () => {
 				askForNext();
 			});
 		} else {
-			console.log('All done with new team members');
-			console.log(employees);
+			console.log('Team complete - rendering team page');
 			const htmlPg = render(employees);
 			writeOutput(htmlPg);
-			console.log(htmlPg);
 		}
 	});
 };
@@ -63,31 +61,10 @@ const buildTeam = () => {
 		const newEmp = new Manager(emp.name, emp.id, emp.email, emp.officeNumber);
 		employees.push(newEmp);
 		containers.ids.push(emp.id);
-		// console.log(employees.filter((emp) => emp.getId() === "1"));
 		askForNext();
 	});
 };
 
-// starts app
+// starts the app
 buildTeam();
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
